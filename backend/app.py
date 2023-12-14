3d024c82a745e1bac9a4b345829db6bf7eb8922e
tomfluff <yotsurfing@gmail.com>
2023-12-14 16:48:42 +0900

    add config

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+import yaml
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -23,6 +23,17 @@ def submit_data():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Load configurations from config.yml
+    with open('config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+    
+    # Verify that the config file is valid
+    assert 'debug' in config, 'debug key not found in config.yml'
+    assert type(config['debug']) == bool, 'debug key must be a boolean'
+
+    # Start the Flask server
+    app.run(debug=config['debug'])
+
+
 app = Flask(__name__)
 
