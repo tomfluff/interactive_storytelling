c0c8579d33bf23d2d6affd47bc42e31c94acb141
Yotam <92244828+tomfluff@users.noreply.github.com>
2024-01-06 19:20:29 +0900

    Refactor code and remove unused components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,54 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import StartPage from "./pages/StartPage/StartPage";
 
-import React, { useEffect, useState } from 'react';
-import WebcamComponent from "./WebcamComponent";
-import StoryComponent from './StoryComponent';
-
-function App() {
-  const [data, setData] = useState<{message: string}>({message: ''});
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/data') // Replace '/api/data' with the actual endpoint of your Flask backend
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error(error));
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from our project! This is the frontend.
-        </p>
-
-        {data && (
-          <p>
-            Data from backend: {data.message}
-          </p>
-        )}
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          <WebcamComponent />
-        </div>
-        <div>
-          <StoryComponent />
-        </div>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <StartPage />;
+};
 
 export default App;
---
+++ frontend/src/__old__/Demo.tsx
@@ -0,0 +1,108 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import useWebcam from "../hooks/useWebcam";
+import useFetchCharacter from "../hooks/useFetchCharacter";
+import Webcam from "react-webcam";
+import { useEffect, useState } from "react";
+import { useDrawingStore } from "../store/drawingStore";
+import useFetchPremise from "../hooks/useFetchPremise";
+import PremiseBar from "../components/PremiseBar/PremiseBar";
+import useAudioStream from "../hooks/useAudioStream";
+import SpeechMonitor from "../components/SpeechMonitor/SpeechMonitor";
+import DrawingScan from "../components/DrawingScan/DrawingScan";
+
+function Demo() {
+  const [sendDrawing, setSendDrawing] = useState<boolean>(false);
+  const drawingData = useDrawingStore.use.drawing();
+  const {
+    isLoading: drawingIsLoading,
+    error: drawingError,
+    fetchCharacter,
+  } = useFetchCharacter();
+  const {
+    isLoading: premiseIsLoading,
+    error: premiseError,
+    data: premises,
+    fetchPremise,
+  } = useFetchPremise();
+  const { audioRef, isPlaying, isStreaming, playAudioStream, stopAudioStream } =
+    useAudioStream();
+
+  useEffect(() => {
+    if (drawingData) {
+      fetchPremise(drawingData);
+    }
+  }, [drawingData]);
+
+  useEffect(() => {
+    console.log(premises);
+  }, [premises]);
+
+  const handlePlayText = () => {
+    if (!drawingData) return;
+
+    playAudioStream(drawingData.character.backstory);
+  };
+
+  return (
+    <Box>
+      <Container>
+        <Typography variant="h1">MyStoryKnight</Typography>
+        <Box sx={{ backgroundColor: "red" }}>
+          <DrawingScan />
+        </Box>
+        <Box>
+          <SpeechMonitor feedback />
+          <Stack direction="row" spacing={2}>
+            <Typography variant="h3">
+              Drawing: {drawingIsLoading ? "Loading..." : "Done"}{" "}
+            </Typography>
+            <Typography variant="h3">
+              Premise: {premiseIsLoading ? "Loading..." : "Done"}{" "}
+            </Typography>
+            <Button variant="contained" onClick={() => setSendDrawing(true)}>
+              Send
+            </Button>
+          </Stack>
+          <Box>
+            <Typography variant="h3">Drawing Error: {drawingError}</Typography>
+            <Typography variant="h3">Premise Error: {premiseError}</Typography>
+            <Typography variant="h2">Drawing</Typography>
+            <Typography variant="h3">
+              Full Name: {drawingData?.character.fullname}
+            </Typography>
+            <Typography variant="h3">
+              Short Name: {drawingData?.character.shortname}
+            </Typography>
+            <Typography variant="h3">
+              Items: {drawingData?.items.join(", ")}
+            </Typography>
+            <Typography variant="h3">
+              Backstory: {drawingData?.character.backstory}
+            </Typography>
+          </Box>
+          <PremiseBar items={premises ?? []} />
+        </Box>
+
+        <Box>
+          <Button variant="contained" onClick={handlePlayText}>
+            Read
+          </Button>
+          <Button variant="contained" onClick={() => stopAudioStream()}>
+            Stop
+          </Button>
+          <Typography variant="h3">
+            Status: {isPlaying ? "Playing" : "Stopped"}
+          </Typography>
+          <Typography variant="h3">
+            Streaming: {isStreaming ? "Streaming" : "Not streaming"}
+          </Typography>
+        </Box>
+        <audio ref={audioRef} />
+      </Container>
+    </Box>
+  );
+}
+
+export default Demo;
